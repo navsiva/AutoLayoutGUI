1194fb34cb5d1631c1b128dc15fa117d07fe4e16
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-06-17 16:51:48 -0400

    Initial Commit

---
+++ AutoLayoutGUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutGUI
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-06-17.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutGUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutGUI
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-06-17.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
