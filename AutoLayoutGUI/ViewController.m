a9ac7160b1db03af544364d2f1cc8809a37d31af
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-06-19 11:31:10 -0400

    AutoLayout GUI code

--- AutoLayoutGUI/ViewController.m
+++ AutoLayoutGUI/ViewController.m
@@ -10,18 +10,29 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *boxView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+
+
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+    
+
+
 
 @end
